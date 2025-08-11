b4aad0bd533f34bcbe84dc4bf672c73840bf2bd7
Vasundhara Devi Potlapinjara <vasundharadevipotlapinjara@gmail.com>
2025-08-11 19:25:34 +0530

    Delete digits.js

--- digits.js
+++
@@ -1,7 +0,0 @@
-let digitNum = parseInt(prompt("Enter a number:"));
-let count = 0;
-while (digitNum > 0) {
-    count++;
-    digitNum = Math.floor(digitNum / 10);
-}
-console.log("Number of digits =", count);
